982bbcdc1a0ffbac860a652f77ed6510e364320c
hg-alan <alan.hg@outlook.com>
2024-01-25 00:28:00 -0500

    widdle away

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
       <div
         style={{
           flex: 1,
-          padding: "1rem",
+          padding: "0.5rem",
           overflowY: "auto",
         }}
       >
@@ -32,8 +32,9 @@ export default function Home() {
           >
             poking the world
           </a>
-          &quot;, so if any of the following resonates with you, let&apos;s
-          chat.
+          &quot;:
+          {/* , so if any of the following resonates with you, let&apos;s
+          chat. */}
         </p>
 
         <br />
@@ -46,29 +47,29 @@ export default function Home() {
             listStylePosition: "outside",
           }}
         >
-          <li style={{ marginTop: "1rem" }}>
+          <li>
             Generative AI{" "}
             <ul style={{ paddingLeft: "20px", marginTop: "0.5rem" }}>
               <li style={{ margin: "0.5rem 0" }}>
-                Web app for creating AI-generated cards:{" "}
+                AI-generated cards:{" "}
                 <a
                   href="https://www.instacard.ai/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   {" "}
-                  instacard.ai
+                  Instacard.ai
                 </a>{" "}
               </li>
               <li style={{ margin: "0.5rem 0" }}>
-                Book examining the intersection of art, technology & human
-                cognition:{" "}
+                Book:{" "}
+                {/* Book examining the intersection of art, technology & human
+                cognition:{" "} */}
                 <a
                   href="https://drive.google.com/file/d/1HZHul6S5f-0NYLbFnGkiQBV5Nm5nsjbO/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  {" "}
                   Do Andoids Dream of Synthetic Images?
                 </a>{" "}
               </li>
@@ -81,23 +82,24 @@ export default function Home() {
               {" "}
               <li style={{ margin: "0.5rem 0" }}>
                 {" "}
-                Clients range from multinational corporations such as{" "}
+                {/* Clients range from multinational corporations such as{" "} */}
+                Clients include:{" "}
                 <a
                   href="https://www.staples.ca/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   Staples
-                </a>{" "}
-                &{" "}
+                </a>
+                ,{" "}
                 <a
                   href="https://www.pernod-ricard.com/en"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  Pernod Ricard
+                  Pernod Ricard,
                 </a>{" "}
-                to brands with cult followings including{" "}
+                {/* to brands with cult followings including{" "} */}
                 <a
                   href="https://quartierishome.com/"
                   target="_blank"
@@ -120,13 +122,13 @@ export default function Home() {
             Photography
             <ul style={{ paddingLeft: "20px", marginTop: "0.5rem" }}>
               <li style={{ margin: "0.5rem 0" }}>
-                Categories include:{" "}
+                Categories:{" "}
                 <a
                   href="https://500px.com/p/alanhg/galleries/cityscape"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  cityscape
+                  Cityscape
                 </a>
                 ,{" "}
                 <a
@@ -134,7 +136,7 @@ export default function Home() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  concert
+                  Concert
                 </a>
                 ,{" "}
                 <a
@@ -142,7 +144,7 @@ export default function Home() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  outside
+                  Outside
                 </a>{" "}
                 &{" "}
                 <a
@@ -150,20 +152,20 @@ export default function Home() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  other
+                  Other
                 </a>
               </li>
             </ul>
           </li>
         </ul>
 
-        <p style={{marginTop:"4rem"}}>
+        {/* <p style={{marginTop:"4rem"}}>
           Don&apos;t panic, <br /> Alan{" "}
-        </p>
+        </p> */}
 
         {/* Social icons */}
         <div
-          style={{ textAlign: "left", marginTop: "4rem", marginBottom: "2rem" }}
+          style={{ textAlign: "left", marginTop: "3rem", marginBottom: "2rem" }}
         >
           <a
             href="mailto:alan.hg@outlook.com"
@@ -174,8 +176,8 @@ export default function Home() {
             <Image
               src="/envelope-regular.svg"
               alt="Your Icon"
-              width={35}
-              height={35}
+              width={27.5}
+              height={27.5}
             />
           </a>
           <a
@@ -187,8 +189,8 @@ export default function Home() {
             <Image
               src="/linkedin-in.svg"
               alt="Your Icon"
-              width={35}
-              height={35}
+              width={27.5}
+              height={27.5}
             />
           </a>
           <a
@@ -200,8 +202,8 @@ export default function Home() {
             <Image
               src="/file-regular.svg"
               alt="Your Icon"
-              width={35}
-              height={35}
+              width={27.5}
+              height={27.5}
             />
           </a>
           <a
@@ -210,7 +212,7 @@ export default function Home() {
             rel="noopener noreferrer"
             style={{ marginRight: "2rem" }}
           >
-            <Image src="/github.svg" alt="Your Icon" width={35} height={35} />
+            <Image src="/github.svg" alt="Your Icon" width={27.5} height={27.5} />
           </a>
         </div>
       </div>
