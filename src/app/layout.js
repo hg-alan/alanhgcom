cb65f688fa3d6546de038474f511f6ad6f8d1295
hg-alan <alan.hg@outlook.com>
2024-02-02 15:27:29 -0500

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head"; // Import Head from next/head
 
-const ibmPlexSans = IBM_Plex_Sans({ subsets: ["latin"], weight: '400'}); 
+const ibmPlexSans = IBM_Plex_Sans({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
   title: "Alan HG",
@@ -13,6 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
       <body className={ibmPlexSans.className}>
         {children}
         <SpeedInsights />
