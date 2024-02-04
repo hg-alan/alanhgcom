b90c6e2d65b0f05dc828a3715f910451e1ba37d8
hg-alan <alan.hg@outlook.com>
2024-02-04 15:01:16 -0500

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,16 +17,29 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link
-          rel="icon"
-          href="/favicon.ico"
-          sizes="16x16 32x32 48x48"
-        />
+        <link rel="icon" href="/favicon.ico" sizes="16x16 32x32 48x48" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a63737" />
+        <meta name="msapplication-TileColor" content="#a63737" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <body className={ibmPlexSans.className}>
         {children}
