dbacf1e74cb699df8327c9b89d5fa31cd0515b52
hg-alan <alan.hg@outlook.com>
2024-02-04 14:23:09 -0500

    favicon test

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head"; 
+import Head from "next/head";
 
 const ibmPlexSans = IBM_Plex_Sans({ subsets: ["latin"], weight: "400" });
 
@@ -17,7 +17,33 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico"/>
+        <link
+          rel="icon"
+          href="/favicons/favicon.ico"
+          sizes="16x16 32x32 48x48"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicons/mstile-144x144.png"
+        />
+        <link
+          rel="icon"
+          sizes="192x192"
+          href="/favicons/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          sizes="512x512"
+          href="/favicons/android-chrome-512x512.png"
+        />
+        <link rel="mask-icon" href="/favicons/favicon.svg" color="#5bbad5" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </Head>
       <body className={ibmPlexSans.className}>
         {children}
