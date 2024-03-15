319a1664872b968840760eeb8e46e8122d6e22c9
hg-alan <alan.hg@outlook.com>
2024-03-14 23:39:37 -0400

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,41 +15,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>Alan HG</title>
+        <meta name="description" content="Alan HG's personal website" />
+
         <meta itemprop="name" content="Alan HG" />
         <meta itemprop="description" content="Alan HG's personal website" />
-        <meta itemprop="image" content="/favicon.ico" />
+        <meta
+          itemprop="image"
+          content="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5e7143fe-27cd-42e9-9994-01023fad5379/d26m3ox-f8a4739f-e0be-41b9-a070-e6358386b84e.jpg/v1/fill/w_589,h_800,q_75,strp/have_you_seen_this_chicken__by_shortwired_d26m3ox-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvNWU3MTQzZmUtMjdjZC00MmU5LTk5OTQtMDEwMjNmYWQ1Mzc5XC9kMjZtM294LWY4YTQ3MzlmLWUwYmUtNDFiOS1hMDcwLWU2MzU4Mzg2Yjg0ZS5qcGciLCJ3aWR0aCI6Ijw9NTg5In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.SNsr4MD7Wlu0msRqYfMiPZP7e_ij7tNng41aMMj0sto"
+        />
+
         <meta property="og:url" content="https://www.alanhg.com" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Alan HG" />
         <meta property="og:description" content="Alan HG's personal website" />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5e7143fe-27cd-42e9-9994-01023fad5379/d26m3ox-f8a4739f-e0be-41b9-a070-e6358386b84e.jpg/v1/fill/w_589,h_800,q_75,strp/have_you_seen_this_chicken__by_shortwired_d26m3ox-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvNWU3MTQzZmUtMjdjZC00MmU5LTk5OTQtMDEwMjNmYWQ1Mzc5XC9kMjZtM294LWY4YTQ3MzlmLWUwYmUtNDFiOS1hMDcwLWU2MzU4Mzg2Yjg0ZS5qcGciLCJ3aWR0aCI6Ijw9NTg5In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.SNsr4MD7Wlu0msRqYfMiPZP7e_ij7tNng41aMMj0sto"
+        />
+
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Alan HG" />
         <meta name="twitter:description" content="Alan HG's personal website" />
-        <meta name="twitter:image" content="/favicon.ico"/>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+        <meta
+          name="twitter:image"
+          content="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5e7143fe-27cd-42e9-9994-01023fad5379/d26m3ox-f8a4739f-e0be-41b9-a070-e6358386b84e.jpg/v1/fill/w_589,h_800,q_75,strp/have_you_seen_this_chicken__by_shortwired_d26m3ox-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvNWU3MTQzZmUtMjdjZC00MmU5LTk5OTQtMDEwMjNmYWQ1Mzc5XC9kMjZtM294LWY4YTQ3MzlmLWUwYmUtNDFiOS1hMDcwLWU2MzU4Mzg2Yjg0ZS5qcGciLCJ3aWR0aCI6Ijw9NTg5In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.SNsr4MD7Wlu0msRqYfMiPZP7e_ij7tNng41aMMj0sto"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a63737" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className={ibmPlexSans.className}>
         {children}
